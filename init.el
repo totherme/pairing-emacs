97ea7be283cecaf625db7227301b3160a797662f
Gareth Smith <gareth@totherme.org>
2018-03-30 17:39:57 +0100

    Refactor: use 'use-package'
    Since we're using use-package, this is also a good opportunity to
    gather all the non-package-related code into a block at the top of the
    init file.

--- init.el
+++ init.el
@@ -2,171 +2,36 @@
 ;; .gitignore
 (setq custom-file "~/.emacs.d/emacs-custom.el")
 
+;; Get packages from melpa-stable
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+
+(package-initialize)
+
+;; Use 'use-package' to manage the getting and loading of emacs
+;; packages
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
 ;; Enable more org-mode export back-ends
 (setq org-export-backends (list 'ascii 'html 'icalendar 'latex 'md 'beamer 'odt))
 
 ;; Make windows-like shortcuts do what most folks expect
 (cua-mode 1)
 
-;; Get packages from melpa-stable
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(package-initialize)
-(package-refresh-contents)
-
-;; Start git or a shell with a single keypress
-(package-install 'magit)
-(global-set-key (kbd "C-x g") 'magit-status)
+;; Start a shell with a single keypress
 (global-set-key (kbd "C-x M-m") 'shell)
-
-;; Make magit (the git UI) work properly with git-duet
-(defcustom git-duet-enabled "best-guess"
-  "Whether or not to use git duet-commit instead of git commit."
-  :group 'git-duet
-  :type '(choice (const "best-guess") (const "enabled") (const "disabled")))
-
-(advice-add 'magit-run-git-with-editor :around
-            'magit-run-git-with-editor--git-duet)
-
-(defun magit-run-git-with-editor--git-duet (fn &rest args)
-  "Wrap magit-run-git-with-editor to use 'duet-commit' instead of 'commit'.
-
-You should pass magit-run-git-with-editor as FN, and any
-remaining args as ARGS."
-    (if (and (git-duet-should-we-use-it?)
-	     (equal (car args) "commit"))
-	(apply fn "duet-commit" (cdr args))
-      (apply fn args)))
-
-(defun git-duet-should-we-use-it? ()
-  "Decide whether or not to use git-duet.
-
-First check the customizable variable git-duet-enabled. If set to
-\"enabled\" then yes. If set to \"disabled\" then no. If set to
-\"best-guess\", try to guess the best option using
-git-duet-available"
-  (or   (equal git-duet-enabled "enabled")
-	(and     (equal git-duet-enabled "best-guess")
-		  (git-duet-available))))
-
-(defun git-duet-available ()
-  "Guess whether git-duet is available on this machine by
-checking for a duet section in ~/.gitconfig"
-  (with-temp-buffer
-    (insert-file "~/.gitconfig")
-    (search-forward "[duet \"env\"]" (point-max) t)))
 
 ;; Display line numbers
 (unless window-system
   (setq linum-format "%3d \u2502 "))
 (global-linum-mode)
-
-;; If shellcheck is in the $PATH, we should use it when we're editing
-;; shell scripts
-(package-install 'flycheck)
-(add-hook 'sh-mode-hook 'flycheck-mode)
-
-;; Enable autocompletion
-(package-install 'company-go)
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-tooltip-limit 20)		; bigger popup window
-(setq company-idle-delay .2) ; decrease delay before autocompletion popup shows
-(global-set-key (kbd "C-c C-n") 'company-complete)
-(global-set-key (kbd "C-c M-n") 'company-complete)
-
-;; Disable company mode when writing plain text, unless you ask for it
-;; with C-c C-n or C-c M-n
-(add-hook 'text-mode-hook (lambda () 
-			    (set (make-local-variable 'company-idle-delay) nil)))
-
-;; Enable on-the-fly syntax and type checking for go files
-(package-install 'go-mode)
-(add-to-list 'load-path "~/.emacs.d/goflymake")
-(require 'go-flymake)
-(add-hook 'go-mode-hook 'flymake-mode)
-(package-install 'go-eldoc)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
-
-;; Enable golang snippits
-(package-install 'yasnippet)
-(require 'yasnippet)
-(add-to-list 'yas-snippet-dirs  "~/.emacs.d/yasnippet-go")
-(yas-global-mode)
-
-;; Enable autocompletion for golang
-(defun my-company-go-backend ()
-                         (set (make-local-variable 'company-backends) '(company-go))
-                          (company-mode))
-(add-hook 'go-mode-hook 'my-company-go-backend)
-
-;; Use goimports instead of gofmt. It's just better.
-(setq gofmt-command "goimports")
-;; ...and gofmt when we save
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-(defun my-golang-introspect (event)
-  "Move the point to the mouse, and try to do godef-jump."
-  (interactive "e")
-  (mouse-set-point event)
-  (godef-jump (point)))
-
-(defun my-do-nothing ()
-    (interactive))
-
-;; Set some keybindings for use in golang files
-(defun my-go-keybindings ()
-  ;; Use M-. (which means Alt-. on practically all keyboards these
-  ;; days) for "go to definition" in go files.
-  (local-set-key (kbd "M-.") 'godef-jump)
-  ;; Use C-c m to trigger a go format
-  (local-set-key (kbd "C-c m") 'gofmt)
-  ;; Use C-c C-e to ask what compile error is under point
-  (local-set-key (kbd "C-c C-e") 'flymake-popup-current-error-menu)
-  (local-set-key (kbd "C-<mouse-1>") 'my-golang-introspect)
-  (local-set-key (kbd "C-<down-mouse-1>") 'my-do-nothing))
-(add-hook 'go-mode-hook 'my-go-keybindings)
-
-;; Make all this fancy golang stuff even when emacs has been started
-;; from the OSX finder
-(when (memq window-system '(mac ns))
-  (package-install 'exec-path-from-shell)
-  (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-env "GOPATH")
-  (exec-path-from-shell-copy-env "PATH"))
-
-;; Move the cursor basically anywhere by mashing the j key
-;; (potentially along with some other key)
-(package-install 'key-chord)
-(package-install 'avy)
-(package-install 'ace-window)
-(key-chord-mode t)
-(key-chord-define-global "jj" 'avy-goto-word-1)
-(key-chord-define-global "jl" 'avy-goto-line)
-(key-chord-define-global "jw" 'ace-window)
-(avy-setup-default)
-
-;; Enable fancy multiple-cursor editing
-(package-install 'multiple-cursors)
-(require 'multiple-cursors)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-;; Use the powerful and intuitive undo-tree instead of powerful but
-;; confusing default undo ring
-(package-install 'undo-tree)
-(global-undo-tree-mode)
-;; Force undo-tree and linum mode to play nice -- stolen from
-;; https://www.emacswiki.org/emacs/UndoTree
-(defun undo-tree-visualizer-update-linum (&rest args)
-    (linum-update undo-tree-visualizer-parent-buffer))
-(advice-add 'undo-tree-visualize-undo :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualize-redo :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualize-undo-to-x :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualize-redo-to-x :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualizer-mouse-set :after #'undo-tree-visualizer-update-linum)
-(advice-add 'undo-tree-visualizer-set :after #'undo-tree-visualizer-update-linum)
 
 ;; Make backup file behaviour more sensible
 (setq
@@ -192,7 +57,191 @@ checking for a duet section in ~/.gitconfig"
 (unless window-system
   (xterm-mouse-mode))
 
+(use-package diminish
+  :ensure t)
+
+;; Use magit for git operations
+(use-package magit
+  :ensure t
+  :pin melpa-stable
+  :commands (magit-status)
+  :bind (("C-x g" . magit-status))
+  :config
+  ;; Make magit work properly with git-duet
+  (defcustom git-duet-enabled "best-guess"
+    "Whether or not to use git duet-commit instead of git commit."
+    :group 'git-duet
+    :type '(choice (const "best-guess") (const "enabled") (const "disabled")))
+
+  (advice-add 'magit-run-git-with-editor :around
+              'magit-run-git-with-editor--git-duet)
+
+  (defun magit-run-git-with-editor--git-duet (fn &rest args)
+    "Wrap magit-run-git-with-editor to use 'duet-commit' instead of 'commit'.
+
+You should pass magit-run-git-with-editor as FN, and any
+remaining args as ARGS."
+    (if (and (git-duet-should-we-use-it?)
+	     (equal (car args) "commit"))
+	(apply fn "duet-commit" (cdr args))
+      (apply fn args)))
+
+  (defun git-duet-should-we-use-it? ()
+    "Decide whether or not to use git-duet.
+
+First check the customizable variable git-duet-enabled. If set to
+\"enabled\" then yes. If set to \"disabled\" then no. If set to
+\"best-guess\", try to guess the best option using
+git-duet-available"
+    (or   (equal git-duet-enabled "enabled")
+	  (and     (equal git-duet-enabled "best-guess")
+		   (git-duet-available))))
+
+  (defun git-duet-available ()
+    "Guess whether git-duet is available on this machine by
+checking for a duet section in ~/.gitconfig"
+    (with-temp-buffer
+      (insert-file "~/.gitconfig")
+      (search-forward "[duet \"env\"]" (point-max) t))))
+
+;; If shellcheck is in the $PATH, we should use it when we're editing
+;; shell scripts
+(use-package flycheck
+  :ensure t
+  :commands flycheck-mode
+  :init
+  (add-hook 'sh-mode-hook 'flycheck-mode))
+
+;; If we want, we can use paredit in lisp buffers
+(use-package paredit
+  :ensure t
+  :commands paredit-mode)
+
+;; Enable autocompletion
+(use-package company-go
+  :ensure t
+  :commands global-company-mode
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (setq company-tooltip-limit 20)		; bigger popup window
+  (setq company-idle-delay .2) ; decrease delay before autocompletion popup shows
+  (global-set-key (kbd "C-c C-n") 'company-complete)
+  (global-set-key (kbd "C-c M-n") 'company-complete)
+
+  ;; Disable company mode when writing plain text, unless you ask for it
+  ;; with C-c C-n or C-c M-n
+  (add-hook 'text-mode-hook (lambda () 
+			      (set (make-local-variable 'company-idle-delay) nil))))
+
+;; Setup all the golang magic
+(use-package go-mode
+  :ensure t
+  :config
+  (use-package go-flymake		; On-the-fly type checking
+    :load-path "~/.emacs.d/goflymake")
+  (add-hook 'go-mode-hook 'flymake-mode)
+  (use-package go-eldoc			; On-the-fly docs
+    :ensure t)
+  (add-hook 'go-mode-hook 'go-eldoc-setup)
+
+  ;; Enable autocompletion for golang
+  (defun my-company-go-backend ()
+    (set (make-local-variable 'company-backends) '(company-go))
+    (company-mode))
+  (add-hook 'go-mode-hook 'my-company-go-backend)
+
+  ;; Use goimports instead of gofmt. It's just better.
+  (setq gofmt-command "goimports")
+  ;; ...and gofmt when we save
+  (add-hook 'before-save-hook 'gofmt-before-save)
+
+  (defun my-golang-introspect (event)
+    "Move the point to the mouse, and try to do godef-jump.
+
+For IDE-like code introspection on mouse events like Control-Click"
+    (interactive "e")
+    (mouse-set-point event)
+    (godef-jump (point)))
+
+  (defun my-do-nothing ()
+    (interactive))
+
+  ;; Set some keybindings for use in golang files
+  (defun my-go-keybindings ()
+    ;; Use M-. (which means Alt-. on practically all keyboards these
+    ;; days) for "go to definition" in go files.
+    (local-set-key (kbd "M-.") 'godef-jump)
+    ;; Use C-c m to trigger a go format
+    (local-set-key (kbd "C-c m") 'gofmt)
+    ;; Use C-c C-e to ask what compile error is under point
+    (local-set-key (kbd "C-c C-e") 'flymake-popup-current-error-menu)
+    ;; Use Control-Click to "go to definition"
+    (local-set-key (kbd "C-<mouse-1>") 'my-golang-introspect)
+    (local-set-key (kbd "C-<down-mouse-1>") 'my-do-nothing))
+  (add-hook 'go-mode-hook 'my-go-keybindings))
+
+;; Enable golang snippits
+(use-package yasnippet
+  :ensure t
+  :config
+  (add-to-list 'yas-snippet-dirs  "~/.emacs.d/yasnippet-go")
+  (yas-global-mode))
+
+
+;; Make all this fancy golang stuff even when emacs has been started
+;; from the OSX finder
+(when (memq window-system '(mac ns))
+  (use-package exec-path-from-shell
+    :ensure t
+    :config
+    (exec-path-from-shell-initialize)
+    (exec-path-from-shell-copy-env "GOPATH")
+    (exec-path-from-shell-copy-env "PATH")))
+
+;; Move the cursor basically anywhere by mashing the j key
+;; (potentially along with some other key)
+(use-package key-chord
+  :ensure t
+  :config
+  (use-package avy
+    :ensure t)
+  (use-package ace-window
+    :ensure t)
+  (key-chord-mode t)
+  (key-chord-define-global "jj" 'avy-goto-word-1)
+  (key-chord-define-global "jl" 'avy-goto-line)
+  (key-chord-define-global "jw" 'ace-window)
+  (avy-setup-default))
+
+;; Enable fancy multiple-cursor editing
+(use-package multiple-cursors
+  :ensure t
+  :config
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
+;; Use the powerful and intuitive undo-tree instead of powerful but
+;; confusing default undo ring
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode)
+  ;; Force undo-tree and linum mode to play nice -- stolen from
+  ;; https://www.emacswiki.org/emacs/UndoTree
+  (defun undo-tree-visualizer-update-linum (&rest args)
+    (linum-update undo-tree-visualizer-parent-buffer))
+  (advice-add 'undo-tree-visualize-undo :after #'undo-tree-visualizer-update-linum)
+  (advice-add 'undo-tree-visualize-redo :after #'undo-tree-visualizer-update-linum)
+  (advice-add 'undo-tree-visualize-undo-to-x :after #'undo-tree-visualizer-update-linum)
+  (advice-add 'undo-tree-visualize-redo-to-x :after #'undo-tree-visualizer-update-linum)
+  (advice-add 'undo-tree-visualizer-mouse-set :after #'undo-tree-visualizer-update-linum)
+  (advice-add 'undo-tree-visualizer-set :after #'undo-tree-visualizer-update-linum))
+
 ;; Make markdown and yaml editing nice
-(package-install 'markdown-mode)
-(package-install 'yaml-mode)
+(use-package markdown-mode
+  :ensure t)
+(use-package yaml-mode
+  :ensure t)
 
